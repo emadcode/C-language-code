3237345a50bc6c6e5b2717572169a60c67a9bd55
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-05-18 19:58:51 +0300

    Add files via upload

---
+++ C 1day/omda.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main() {
+    printf("Hello, Emad How are you\n");// outbut hello, emad how are you 
+    // write ./omda if you want print enything
+    return 0;
+}
+
+//#include <stdio.h>: include the standard input/output library.
+  //  int main (): the starting point for execution.
+    //printf: a function for printing text to the screen.
+    //return 0;: successfully terminated the program.
+
+//2. Variants and types
+//Basic types
+
+  //  int: for integers.
+    //float: for decimal numbers with low precision.
+    //double: for decimal numbers with high precision.
+    //char: for one character.
