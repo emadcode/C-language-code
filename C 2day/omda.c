1b0c9ffc62ca37dfa2a7f1efe50314f441025854
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-05-19 19:57:25 +0300

    Add files via upload

---
+++ C 2day/omda.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main() {
+    int o = 0;
+
+    while (o < 10) {
+        printf("o = %d\n", o);
+        o++;
+    }
+    //"Here, the numbers will start from 1 to 9 i."
+    
+    return 0;
+}
